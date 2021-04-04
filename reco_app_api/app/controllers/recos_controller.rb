6799cbfb5092e419503a50c5962c07908ba7a10c
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-04 17:36:31 +1000

    added create function logic to add new reco

--- reco_app_api/app/controllers/recos_controller.rb
+++ reco_app_api/app/controllers/recos_controller.rb
@@ -1,10 +1,20 @@
 class RecosController < ApplicationController
     def index
-        render json: { message: 'INDEX' }
+        # render json: Reco.all
+        render json: { message: 'TEST' }
+
     end
 
     def create
-        render json: { message: 'CREATE' }
+        reco = Reco.create(category: params[:category], name: params[:name], cost: params[:cost], source: params[:source], description: params[:description])
+        reco_valid = reco.valid?
+        p reco
+        p reco_valid
+        if reco_valid
+            render json: {message: 'Successfully created new recommendation!'}, status: 200
+        else
+            render json: {message: 'Unable to create new recommendation'}, status: 400
+        end
     end
 
     def show
--- reco_app_api/app/models/reco.rb
+++ reco_app_api/app/models/reco.rb
@@ -1,3 +1,2 @@
 class Reco < ApplicationRecord
-    
 end

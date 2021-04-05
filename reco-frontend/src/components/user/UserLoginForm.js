79a34beafc2d0a7166e1986b61c37e10fd001b5c
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-05 10:12:53 +1000

    added handle change logic

--- reco-frontend/src/components/user/UserLoginForm.js
+++ reco-frontend/src/components/user/UserLoginForm.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 export const UserLoginForm = () => {
+
+    const [form, setForm] = useState({
+        name: '',
+        password: ''
+    })
+
+    const changeHandler = (e) => {
+        const newFormState = { ...form };
+        newFormState[e.target.name] = e.target.value;
+        setForm(newFormState);
+    }
+
     return (
         <div>
           <h1>Login</h1>
           <form>
             <label>
               Username:
-              <input name="name" />
+              <input name="name" value={form.name} onChange={changeHandler} />
             </label>
             <label>
               Password:
-              <input name="password" />
+              <input name="password" value={form.password} onChange={changeHandler} />
             </label>
             <button type="submit" >Submit</button>
           </form>

f4980ddc983245157ec86167939947060342121a
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-21 12:25:23 +1000

    add get route

--- reco-backend/src/controllers/recoController.js
+++ reco-backend/src/controllers/recoController.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const Reco = require('../models/RecoModel');
 
 const router = express.Router();
+
+// router.get("/", (req, res) => res.send("this is the recos router"));
+
+router.get('/', async (req, res, next) => {
+    console.log(req.body, ' this is get all recos')
+       try  {
+  
+        const allRecos = await Reco.find();
+  
+        res.json({
+          status: {
+              code: 200,
+              message: "Success"
+            },
+          data: allRecos
+        });
+  
+      } catch (err){
+  
+        res.send(err)
+  
+      }
+  });
+
 
 module.exports = router;

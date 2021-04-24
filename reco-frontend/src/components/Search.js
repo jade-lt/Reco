29606a950d48b57697219288ef49bf654150039c
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-24 18:09:03 +1000

    add search feature

--- reco-frontend/src/components/Search.js
+++ reco-frontend/src/components/Search.js
@@ -1,7 +1,74 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useHistory } from "react-router";
+
+
+
 export const Search = () => {
-    return (
-        <div>
-            <h1>This is the search page</h1>
-        </div>
-    )
+
+  const history = useHistory();
+
+
+    const [data ,setData] = useState([]);
+    const [filtered ,setFilterd] = useState([]);
+    const [result , setResult] = useState("");
+
+    useEffect(()=>{
+        const fetchData = async ()=> {
+                try{
+                    const res = await axios.get('/api/recos');
+                    setData(res.data);
+                    setFilterd(res.data);
+                }catch(err){
+                    throw new Error(err);
+                }
+                 };
+              fetchData(); 
+    },[]);
+
+
+
+
+  useEffect(()=> {
+    const results = filtered.filter(res=> res.name.toLowerCase().includes(result)
+
+    ); 
+    setData(results)
+} ,[result])
+
+
+
+const onChange =(e)=> {
+    setResult(e.target.value);
+}
+
+return (
+<div>
+<input 
+    type="text"
+    placeholder="serch here .."
+    value={result}
+    onChange={onChange}
+/>
+<ul>
+{data.map((el) => (
+          <div className="user-recos-list" id="all-recos-hoverable">
+            <div className={`${el.category}-category`} onClick={() => history.push(`/reco/${el._id}`)}>
+            <li key={el._id}>
+              <h5 className="reco-name">{el.name}</h5>
+              <img className="reco-img" src={el.img} alt=""></img>
+              <br />
+              Category: {el.category}
+              <br />
+              Source/Author: {el.source}
+            </li>
+          </div>
+          </div>
+        ))}
+        </ul>
+</div>
+
+)  
+
+    
 }

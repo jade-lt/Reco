023bcfb3ac1cb41db90609fc81b812ff89aba3d2
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-29 21:57:12 +1000

    data from api captured/renders

--- reco-frontend/src/components/reco/RecoCreateViaApi.js
+++ reco-frontend/src/components/reco/RecoCreateViaApi.js
@@ -6,13 +6,6 @@ import Button from "react-bootstrap/Button";
 export const RecoCreateViaApi = () => {
   //   const history = useHistory();
 
-  const [form, setForm] = useState({
-    name: "",
-    category: "",
-  });
-
-  const [searchResults, setSearchResults] = useState("");
-
   //   const [reco, setReco] = useState({
   //     category: "",
   //     name: "",
@@ -23,60 +16,12 @@ export const RecoCreateViaApi = () => {
   //     img: "",
   //   });
 
-  const nameInput = form.name;
-  const categoryInput = form.category.toLowerCase();
+  const [searchResults, setSearchResults] = useState([]);
 
-  const inputUrl = encodeURI(
-    `http://api.themoviedb.org/3/search/${categoryInput}?api_key=dd5b5fb0f236579b40c792f17042106b&query=${nameInput}`
-  );
-
-  console.log(inputUrl);
-
-  //   useEffect(() => {
-  //     fetch(inputUrl, {
-  //       headers: {
-  //         token: window.localStorage.getItem("token"),
-  //       },
-  //     })
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //           if(data.length) {
-  //             setSearchResults(data);
-  //             console.log(searchResults)
-  //           } else {
-  //               console.log("couldnt set search results")
-  //           }
-
-  //         }
-  //       )
-  //       .catch((error) => console.log("catch error:", error));
-  //   }, []);
-
-  //   const [searchApi, setSearchApi] = useEffecT()
-
-  //   setReco({
-  //     category: form.input,
-  //     name: form.input,
-  //     cost: form.input,
-  //     source: form.input,
-  //     description: form.input,
-  //     genre: form.input,
-  //     img: form.input,
-  //   })
-
-  // const queryInput = "the matrix"
-
-  //   const searchApi = async (inputUrl) => {
-  //       const response = await fetch(`https://api.themoviedb.org/3/search/keyword?api_key=dd5b5fb0f236579b40c792f17042106b&query=${queryInput}`)
-  //       const json = await response.json()
-  //       setSearchApi({ data:json, loading:false })
-  //   }
-
-  // useEffect(() => {
-  //     if (search !== '') {
-  //         searchApi(`https://api.themoviedb.org/3/search/keyword?api_key=dd5b5fb0f236579b40c792f17042106b&query=${queryInput}`)
-  //     }
-  // }, [search])
+  const [form, setForm] = useState({
+    name: "",
+    category: "",
+  });
 
   const changeHandler = (e) => {
     const newFormState = { ...form };
@@ -84,52 +29,29 @@ export const RecoCreateViaApi = () => {
     setForm(newFormState);
   };
 
-  //   const queryInput = form.input
-  //   console.log(queryInput)
+  useEffect(() => {});
+
+  const nameInput = form.name;
+  const categoryInput = form.category.toLowerCase();
+
+  const inputUrl = encodeURI(
+    `http://api.themoviedb.org/3/search/${categoryInput}?api_key=dd5b5fb0f236579b40c792f17042106b&query=${nameInput}`
+  );
 
   const submitSearchHandler = (e) => {
     e.preventDefault();
+    console.log("api search button was clicked");
+
     fetch(inputUrl)
       .then((res) => res.json())
       .then((data) => {
-        console.log("Output: ", data);
+        console.log("Array of results: ", data.results);
+        const array = data.results;
+        setSearchResults(array);
+        console.log("setSearchResults ", searchResults);
       })
       .catch((err) => console.error(err));
-    // fetch("/api/recos", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     token: window.localStorage.getItem("token"),
-    //   },
-    //   body: JSON.stringify(reco)
-    // })
-    //   .then((response) => response.json())
-    //   .then(history.push("/my-recos"));
   };
-
-  //     useEffect(() => {
-  //         fetch("/tmdb/3/search/keyword?api_key=dd5b5fb0f236579b40c792f17042106b&query=matrix&page=1", {
-
-  //             headers: {
-  //                 'token': window.localStorage.getItem('token')
-  //               }
-  // })
-  // .then(response => {
-  // 	console.log(response);
-  // })
-  // .catch(err => {
-  // 	console.error(err);
-  // });
-  //     })
-
-  // for dealing with spaces later
-  // encodeURI("http://www.example.org/a file with spaces.html")
-  // http://www.example.org/a%20file%20with%20spaces.html
-
-  // .then(data => setResult(data))
-  // .catch(err => {
-  // 	console.error(err);
-  // });
 
   return (
     <div>
@@ -152,7 +74,22 @@ export const RecoCreateViaApi = () => {
         </Button>
       </form>
       <div>
-        <p>{searchResults}</p>
+        <ul>
+          {searchResults.map((el) => (
+            <div className="user-recos-list" id="all-recos-hoverable">
+              <li key={el.id}>
+                <h4>{el.original_title}</h4>
+                <img
+                  className="reco-img"
+                  src={`https://image.tmdb.org/t/p/w200${el.poster_path}`}
+                  alt=""
+                ></img>
+                {/* <br />
+                {el.overview} */}
+              </li>
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- reco-frontend/src/setupProxy.js
+++ reco-frontend/src/setupProxy.js
@@ -1,20 +1,7 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:9000',
-      changeOrigin: true,
-    })
-  );
+    app.use(proxy('/api', { target: 'https:/localhost/9000' }));
 
-  app.use(
-    '/tmdb',
-    createProxyMiddleware({
-      target: 'http://api.themoviedb.org',
-      changeOrigin: true,
-    })
-  );
-
-};
+    app.use(proxy('/tmdb', { target: 'https://api.themoviedb.org/' }));
+}

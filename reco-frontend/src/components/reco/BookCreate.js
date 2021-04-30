c0f74993adfa58b84e29ba014d4dfcb0c4ab4777
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-30 18:39:44 +1000

    book api fetch/post working

--- reco-frontend/src/components/reco/BookCreate.js
+++ reco-frontend/src/components/reco/BookCreate.js
@@ -8,15 +8,7 @@ export const BookCreate = () => {
     const history = useHistory();
 
 
-    const [reco, setReco] = useState({
-        category: "",
-        name: "",
-        cost: "",
-        source: "",
-        description: "",
-        genre: "",
-        img: "",
-      });
+
   
     const [searchResults, setSearchResults] = useState([]);
   
@@ -36,19 +28,32 @@ export const BookCreate = () => {
     const nameInput = form.name;
 
   const inputUrl = (
-    `https://openlibrary.org/search.json?q=${nameInput}`
+    `https://www.googleapis.com/books/v1/volumes?q=${nameInput}&key=AIzaSyD0fhiuvUpOW2Y0zysgECZCG1tofGSv6Jk`
   );
 
   const submitSearchHandler = (e) => {
     e.preventDefault();
     console.log("api search button was clicked");
+    console.log(inputUrl);
 
     fetch(inputUrl)
       .then((res) => res.json())
       .then((data) => {
         console.log("data: ", data);
-        const array = data.docs;
+        const array = data.items;
         console.log("array: ", array);
+        const bookImage = array[0].volumeInfo.imageLinks
+        
+        console.log("image: ", bookImage);
+        console.log("name: ", array[0].volumeInfo.title);
+
+        console.log("genre: ", array[0].volumeInfo.categories[0]);
+        console.log("description: ", array[0].volumeInfo.description);
+    
+        const thumbnail = bookImage.thumbnail
+        console.log("thumbnail", thumbnail);
+
+
 
         setSearchResults(array);
         // console.log("setSearchResults ", searchResults);
@@ -71,7 +76,6 @@ export const BookCreate = () => {
          
           <input name="name" value={form.name} placeholder="Enter a title or author" onChange={changeHandler} />
         </label>
-       
         <Button variant="primary" type="submit">
           Search
         </Button>
@@ -81,33 +85,39 @@ export const BookCreate = () => {
           {searchResults.map((el) => (
             <div className="user-recos-list" id="all-recos-hoverable" >
               <li key={el.id_google}>
-                <h4>{el.title}</h4>
-                {/* <h4>{el.name}</h4> */}
+                <h4>{el.volumeInfo.title}</h4>
 
-                {/* <img
+                <img
                   className="reco-img"
-                  src={`https://image.tmdb.org/t/p/w200/${el.poster_path}`}
+                  src={el.volumeInfo.imageLinks.thumbnail}
                   alt="">
-                  </img> */}
-
+                  </img>
+<br />
 
 
                   <Button variant="primary" 
-                  
-                //   onClick={() => {
+                   onClick={() => {
+
                     
-                //       setReco({
-                //         category: form.category,
-                //         name: el.original_title,
-                //         cost: "",
-                //         source: "",
-                //         description: el.overview,
-                //         genre: "",
-                //         img: `https://image.tmdb.org/t/p/w200${el.poster_path}`,
-                //     })
-                //     history.push('/test')
-                //     }
-                //   }
+                    fetch("/api/recos", {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json",
+                        token: window.localStorage.getItem("token"),
+                      },
+                      body: JSON.stringify({
+                        category: "Book",
+                        name: el.volumeInfo.title,
+                        cost: "",
+                        source: "",
+                        description: el.volumeInfo.description,
+                        genre: el.volumeInfo.categories[0],
+                        img: el.volumeInfo.imageLinks.thumbnail,
+                      }),
+                    })
+                      .then((response) => response.json())
+                      .then(history.push("/my-recos"));
+                  }}
                   
                     
 

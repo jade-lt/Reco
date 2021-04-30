67746add43d0980735c0b6c7e9aeaac24a360c0c
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-30 11:23:14 +1000

    reco can be created by searching api or manually

--- reco-frontend/src/components/reco/RecoCreate.js
+++ reco-frontend/src/components/reco/RecoCreate.js
@@ -1,88 +1,108 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
-import Button from 'react-bootstrap/Button';
 
-
-
+import Button from "react-bootstrap/Button";
 
 export const RecoCreate = () => {
-
-    const [reco, setReco] = useState({
-        category: '',
-        name: '',
-        cost: '',
-        source: '',
-        description: '',
-        genre: '',
-        img: ''
-    })
-
     const history = useHistory();
 
-    const changeHandler = (e) => {
-        const newFormState = { ...reco };
-        newFormState[e.target.name] = e.target.value;
-        setReco(newFormState);
-    }
+  //   const [reco, setReco] = useState({
+  //     category: "",
+  //     name: "",
+  //     cost: "",
+  //     source: "",
+  //     description: "",
+  //     genre: "",
+  //     img: "",
+  //   });
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        fetch('/api/recos', {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-            'token': window.localStorage.getItem('token')
-          },
-          body: JSON.stringify(reco)
-        })
-          .then(response => response.json())
-          .then(history.push('/my-recos'))
-      }
+  const [searchResults, setSearchResults] = useState([]);
 
-      const clickCancelHandler = () => {
-        history.replace("/my-recos");
-      };
+  const [form, setForm] = useState({
+    name: "",
+    category: "",
+  });
 
-      return (
-          
-        <div className="main">
-            <div className="header-text" id="header-create">
-                <h1>Add a new Reco</h1>
-                </div>
-                <form onSubmit={submitHandler}>
-                    <label>
-                        Category<br />
-                        <input name="category" value={reco.category} onChange={changeHandler} />
-                    </label><br />
-                    <label>
-                        Name<br />
-                        <input name="name" value={reco.name} onChange={changeHandler} />
-                    </label><br />
-                    <label>
-                        Genre<br />
-                        <input name="genre" value={reco.genre} onChange={changeHandler} />
-                    </label><br />
-                    <label>
-                        Cost<br />
-                        <input name="cost" value={reco.cost} onChange={changeHandler} />
-                    </label><br />
-                    <label>
-                        Source/Author<br />
-                        <input name="source" value={reco.source} onChange={changeHandler} />
-                    </label><br />
-                    <label>
-                        Description/Comment<br />
-                        <input name="description" value={reco.description} onChange={changeHandler} />
-                    </label><br />
-                    <label>
-                    Image Url<br />
-                    <input name="img" value={reco.img} onChange={changeHandler} />
-                </label><br />
-                <Button variant="outline-primary" onClick={clickCancelHandler} >Cancel</Button>
+  const changeHandler = (e) => {
+    const newFormState = { ...form };
+    newFormState[e.target.name] = e.target.value;
+    setForm(newFormState);
+  };
 
-                    <Button variant="primary" type="submit">Save</Button>
-    
-                </form>
+  useEffect(() => {});
+
+  const nameInput = form.name;
+  const categoryInput = form.category.toLowerCase();
+
+  const inputUrl = encodeURI(
+    `http://api.themoviedb.org/3/search/${categoryInput}?api_key=dd5b5fb0f236579b40c792f17042106b&query=${nameInput}`
+  );
+
+  const submitSearchHandler = (e) => {
+    e.preventDefault();
+    console.log("api search button was clicked");
+
+    fetch(inputUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Array of results: ", data.results);
+        const array = data.results;
+        setSearchResults(array);
+        console.log("setSearchResults ", searchResults);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const clickAddManuallyHandler = () => {
+      history.push('/reco/add-manually')
+  }
+
+  return (
+    <div>
+      <h3>Search for a new Reco to add</h3>
+      <form onSubmit={submitSearchHandler}>
+        <label>
+          Name:
+          <input name="name" value={form.name} onChange={changeHandler} />
+        </label>
+        <label>
+          Category:
+          <input
+            name="category"
+            value={form.category}
+            onChange={changeHandler}
+          />
+        </label>
+        <Button variant="primary" type="submit">
+          Search
+        </Button>
+      </form>
+      <div>
+        <ul>
+          {searchResults.map((el) => (
+            <div className="user-recos-list" id="all-recos-hoverable" >
+              <li key={el.id}>
+                <h4>{el.original_title}</h4>
+                <h4>{el.name}</h4>
+
+                <img
+                  className="reco-img"
+                  src={`https://image.tmdb.org/t/p/w200${el.poster_path}`}
+                  alt="">
+                  </img>
+              </li>
             </div>
-        )
-}
+          ))}
+        </ul>
+        <br />
+      </div>
+      <div>
+
+      <h3>Or add a new Reco manually</h3>
+      <Button variant="primary" onClick={clickAddManuallyHandler}>
+          Manualy Add a Reco
+        </Button>
+        </div>
+    </div>
+  );
+};
---
+++ reco-frontend/src/components/reco/RecoCreateManually.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+import Button from 'react-bootstrap/Button';
+
+
+
+
+export const RecoCreateManually = () => {
+
+    const [reco, setReco] = useState({
+        category: '',
+        name: '',
+        cost: '',
+        source: '',
+        description: '',
+        genre: '',
+        img: ''
+    })
+
+    const history = useHistory();
+
+    const changeHandler = (e) => {
+        const newFormState = { ...reco };
+        newFormState[e.target.name] = e.target.value;
+        setReco(newFormState);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        fetch('/api/recos', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'token': window.localStorage.getItem('token')
+          },
+          body: JSON.stringify(reco)
+        })
+          .then(response => response.json())
+          .then(history.push('/my-recos'))
+      }
+
+      const clickCancelHandler = () => {
+        history.replace("/my-recos");
+      };
+
+      return (
+          
+        <div className="main">
+            <div className="header-text" id="header-create">
+                <h1>Add a new Reco</h1>
+                </div>
+                <form onSubmit={submitHandler}>
+                    <label>
+                        Category<br />
+                        <input name="category" value={reco.category} onChange={changeHandler} />
+                    </label><br />
+                    <label>
+                        Name<br />
+                        <input name="name" value={reco.name} onChange={changeHandler} />
+                    </label><br />
+                    <label>
+                        Genre<br />
+                        <input name="genre" value={reco.genre} onChange={changeHandler} />
+                    </label><br />
+                    <label>
+                        Cost<br />
+                        <input name="cost" value={reco.cost} onChange={changeHandler} />
+                    </label><br />
+                    <label>
+                        Source/Author<br />
+                        <input name="source" value={reco.source} onChange={changeHandler} />
+                    </label><br />
+                    <label>
+                        Description/Comment<br />
+                        <input name="description" value={reco.description} onChange={changeHandler} />
+                    </label><br />
+                    <label>
+                    Image Url<br />
+                    <input name="img" value={reco.img} onChange={changeHandler} />
+                </label><br />
+                <Button variant="outline-primary" onClick={clickCancelHandler} >Cancel</Button>
+
+                    <Button variant="primary" type="submit">Save</Button>
+    
+                </form>
+            </div>
+        )
+}

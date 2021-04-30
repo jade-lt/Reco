8c5ff8388da92130c388bae7ae34940f01f4c8c5
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-30 11:21:56 +1000

    fixed proxy issue

--- reco-frontend/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = function(app) {
-    app.use(proxy('/api', { target: 'https:/localhost/9000' }));
-
-    app.use(proxy('/tmdb', { target: 'https://api.themoviedb.org/' }));
-}

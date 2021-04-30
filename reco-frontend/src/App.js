14fee8bb0317500730b82290722d0707919b4927
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-30 11:24:29 +1000

    update routes/imports to reflect file changes

--- reco-frontend/src/App.js
+++ reco-frontend/src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 import { UserLoginForm } from "./components/user/UserLoginForm";
 import { RecoUpdate } from "./components/reco/RecoUpdate";
 import { MyRecos } from "./components/user/MyRecos";
-import { RecoCreate } from "./components/reco/RecoCreate";
+import { RecoCreateManually } from "./components/reco/RecoCreateManually";
 import { HomePage } from "./components/HomePage";
 import { UserRegister } from "./components/user/UserRegister";
 import { RecoDelete } from "./components/reco/RecoDelete";
@@ -17,7 +17,7 @@ import { Navbar } from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { MyList } from "./components/user/MyList";
 import { UserDashboard } from "./components/user/UserDashboard";
-import { AllRecos } from "./components/AllRecos"
+import { AllRecos } from "./components/AllRecos";
 import { RecoInfo } from "./components/reco/RecoInfo";
 import { AllClubs } from "./components/club/AllClubs";
 import { ClubInfo } from "./components/club/ClubInfo";
@@ -28,24 +28,22 @@ import { AllGenres } from "./components/genre/AllGenres";
 import { Genre } from "./components/genre/Genre";
 import { Category } from "./components/category/Category";
 import { ClubChat } from "./components/club/ClubChat";
-import { RecoCreateViaApi } from "./components/reco/RecoCreateViaApi";
-
+import { RecoCreate } from "./components/reco/RecoCreate";
+import { ClubMembership } from "./components/club/ClubMembership";
 
 function App() {
-
   const isLoggedIn = () => {
     const token = window.localStorage.getItem("token");
-  
+
     if (token) {
       const decoded = jwt(token);
       const expires = moment.unix(decoded.exp);
-  
+
       return moment().isBefore(expires);
     } else {
       return false;
     }
   };
-
 
   const [loggedIn, setLoggedIn] = useState(isLoggedIn);
 
@@ -54,26 +52,9 @@ function App() {
       <div className="App">
         <Navbar setLoginStatus={setLoggedIn} loginStatus={loggedIn} />
         <Switch>
-
-
-
-
-        <Route exact path="/test">
-            <RecoCreateViaApi />
+          <Route exact path="/reco/add-manually">
+            <RecoCreateManually />
           </Route>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
           <Route exact path="/login">
             <UserLoginForm setLoginStatus={setLoggedIn} />
@@ -144,58 +125,47 @@ function App() {
           </Route>
 
           <Route exact path="/Action-genre">
-          <Genre genre="Action" />
-
+            <Genre genre="Action" />
           </Route>
 
           <Route exact path="/Crime-genre">
-          <Genre genre="Crime" />
-
+            <Genre genre="Crime" />
           </Route>
 
           <Route exact path="/Drama-genre">
-          <Genre genre="Drama" />
-
+            <Genre genre="Drama" />
           </Route>
 
           <Route exact path="/Fantasy-genre">
-          <Genre genre="Fantasy" />
-
+            <Genre genre="Fantasy" />
           </Route>
 
           <Route exact path="/Horror-genre">
-          <Genre genre="Horror" />
-
+            <Genre genre="Horror" />
           </Route>
 
           <Route exact path="/Non-Fiction-genre">
-          <Genre genre="Non-Fiction" />
-
+            <Genre genre="Non-Fiction" />
           </Route>
 
           <Route exact path="/Other-genre">
-          <Genre genre="Other" />
-
+            <Genre genre="Other" />
           </Route>
 
           <Route exact path="/Romance-genre">
-          <Genre genre="Romance" />
-
+            <Genre genre="Romance" />
           </Route>
 
           <Route exact path="/Sci-Fi-genre">
-          <Genre genre="Sci-Fi" />
-
+            <Genre genre="Sci-Fi" />
           </Route>
 
           <Route exact path="/Thriller-genre">
-          <Genre genre="Thriller" />
-
+            <Genre genre="Thriller" />
           </Route>
 
           <Route exact path="/Western-genre">
-          <Genre genre="Western" />
-
+            <Genre genre="Western" />
           </Route>
 
           <Route exact path="/categories">
@@ -208,23 +178,22 @@ function App() {
           </Route>
 
           <Route exact path="/all-categories/Movie">
-          <h1>Movies</h1>
+            <h1>Movies</h1>
 
             <Category category="Movie" />
           </Route>
 
           <Route exact path="/all-categories/TV">
-          <h1>TV Shows</h1>
+            <h1>TV Shows</h1>
 
             <Category category="TV" />
           </Route>
 
           <Route exact path="/all-categories/Game">
-          <h1>Games</h1>
-            
+            <h1>Games</h1>
+
             <Category category="Game" />
           </Route>
-          
 
           <Route exact path="/all-clubs">
             <AllClubs />
@@ -235,24 +204,47 @@ function App() {
           </Route>
 
           <Route exact path="/Book-Club">
-          <Category category="Book" />
-          <ClubChat club="Book" username="jade1234" />
+            <ClubMembership
+              category="Book"
+              name="Book Club"
+              description="a club for people to find and chat about books"
+              img="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_323457.png&f=1&nofb=1"
+            />
+            <Category category="Book" />
+            <ClubChat club="Book" username="jade1234" />
           </Route>
 
           <Route exact path="/Movie-Club">
+          <ClubMembership
+              category="Movie"
+              name="Movie Club"
+              description="a club for people to find and chat about movies"
+              img="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2018%2F06%2FCinema.png&f=1&nofb=1"
+            />
             <Category category="Movie" />
-          <ClubChat club="Movie" username="jade1234" />
-
+            <ClubChat club="Movie" username="jade1234" />
           </Route>
 
           <Route exact path="/TV-Club">
-          <Category category="TV" />
-          <ClubChat club="TV" username="jade1234" />
+          <ClubMembership
+              category="TV"
+              name="TV Club"
+              description="a club for people to find and chat about tv shows"
+              img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpurepng.com%2Fpublic%2Fuploads%2Flarge%2Fpurepng.com-old-televisiontvtelecommunicationmonochromeblack-and-whittelevisionoldblack-and-whiteclipart-1421526536100hrgmw.png&f=1&nofb=1"
+            />
+            <Category category="TV" />
+            <ClubChat club="TV" username="jade1234" />
           </Route>
 
           <Route exact path="/Game-Club">
-          <Category category="Game" />
-          <ClubChat club="Game" username="jade1234" />
+          <ClubMembership
+              category="Game"
+              name="Game Club"
+              description="a club for people to find and chat about games"
+              img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fa%2Fa7%2FVideo_game_controller_icon_designed_by_Maico_Amorim.svg%2F937px-Video_game_controller_icon_designed_by_Maico_Amorim.svg.png&f=1&nofb=1"
+            />
+            <Category category="Game" />
+            <ClubChat club="Game" username="jade1234" />
           </Route>
 
           <Route exact path="/my-club/delete/:id">

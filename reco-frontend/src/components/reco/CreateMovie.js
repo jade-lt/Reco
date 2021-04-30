747e2d5c8459a15fd03b450477afb678502d7c44
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-04-30 12:17:50 +1000

    movie recos from api now post to mongodb

--- reco-frontend/src/components/reco/CreateMovie.js
+++ reco-frontend/src/components/reco/CreateMovie.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router";
 
 import Button from "react-bootstrap/Button";
 
-export const RecoCreate = () => {
+export const CreateMovie = () => {
     const history = useHistory();
 
     const [reco, setReco] = useState({
@@ -20,7 +20,6 @@ export const RecoCreate = () => {
 
   const [form, setForm] = useState({
     name: "",
-    category: "",
   });
 
   const changeHandler = (e) => {
@@ -32,10 +31,9 @@ export const RecoCreate = () => {
   useEffect(() => {});
 
   const nameInput = form.name;
-  const categoryInput = form.category.toLowerCase();
 
   const inputUrl = encodeURI(
-    `http://api.themoviedb.org/3/search/${categoryInput}?api_key=dd5b5fb0f236579b40c792f17042106b&query=${nameInput}`
+    `http://api.themoviedb.org/3/search/movie?api_key=dd5b5fb0f236579b40c792f17042106b&query=${nameInput}`
   );
 
   const submitSearchHandler = (e) => {
@@ -53,46 +51,34 @@ export const RecoCreate = () => {
       .catch((err) => console.error(err));
   };
 
-  const clickAddManuallyHandler = () => {
-      history.push('/reco/add-manually')
+  const clickAddToMyRecosHandler = () => {
+    fetch('/api/recos', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'token': window.localStorage.getItem('token')
+      },
+      body: JSON.stringify({
+        category: reco.category,
+        name: reco.name,
+        cost: reco.cost,
+        source: reco.source,
+        description: reco.description,
+        genre: reco.genre,
+        img: reco.img,
+    })
+    })
+      .then(response => response.json())
+      .then(history.push('/my-list'))
   }
-
-  // const clickAddToMyRecosHandler = () => {
-  //   fetch('/api/recos', {
-  //     method: "POST",
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'token': window.localStorage.getItem('token')
-  //     },
-  //     body: JSON.stringify({
-  //       category: reco.category,
-  //       name: reco.name,
-  //       cost: reco.cost,
-  //       source: reco.source,
-  //       description: reco.description,
-  //       genre: reco.genre,
-  //       img: reco.img,
-  //   })
-  //   })
-  //     .then(response => response.json())
-  //     .then(history.push('/my-list'))
-  // }
 
   return (
     <div>
-      <h3>Search for a new Reco to add</h3>
+      <h3>Add a Movie Reco</h3>
       <form onSubmit={submitSearchHandler}>
         <label>
           Name:
-          <input name="name" value={form.name} onChange={changeHandler} />
-        </label>
-        <label>
-          Category:
-          <input
-            name="category"
-            value={form.category}
-            onChange={changeHandler}
-          />
+          <input name="name" value={form.name} placeholder="Enter a title here" onChange={changeHandler} />
         </label>
         <Button variant="primary" type="submit">
           Search
@@ -104,7 +90,6 @@ export const RecoCreate = () => {
             <div className="user-recos-list" id="all-recos-hoverable" >
               <li key={el.id}>
                 <h4>{el.original_title}</h4>
-                <h4>{el.name}</h4>
 
                 <img
                   className="reco-img"

35ae6faa6225e78d98490e82d84931f0f8c8c020
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-05-04 12:10:51 +1000

    remove test components

--- reco-frontend/src/components/RecoListTest.js
+++ reco-frontend/src/components/RecoListTest.js
@@ -1,26 +1,26 @@
-import { RecoCard } from "./RecoCard";
-import { useEffect, useState } from "react";
+// import { RecoCard } from "./reco/RecoCard";
+// import { useEffect, useState } from "react";
 
-export const RecoListTest = () => {
-  const [recos, setRecos] = useState([]);
+// export const RecoListTest = () => {
+//   const [recos, setRecos] = useState([]);
 
-  useEffect(() => {
-    fetch("/api/recos", {
-      headers: {
-        token: window.localStorage.getItem("token"),
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const currentUserId = window.localStorage.getItem("id");
-        const results = data.filter(
-          (result) => result.userId === currentUserId
-        );
+//   useEffect(() => {
+//     fetch("/api/recos", {
+//       headers: {
+//         token: window.localStorage.getItem("token"),
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const currentUserId = window.localStorage.getItem("id");
+//         const results = data.filter(
+//           (result) => result.userId === currentUserId
+//         );
 
-        setRecos(results);
-      })
-      .catch((error) => console.log("catch error:", error));
-  }, []);
+//         setRecos(results);
+//       })
+//       .catch((error) => console.log("catch error:", error));
+//   }, []);
 
-  return <RecoCard listToMap={recos} />;
-};
+//   return <RecoCard listToMap={recos} />;
+// };

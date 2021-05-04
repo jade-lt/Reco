c8cc95a90ae1880ff080fc648a16d6558ff3237f
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-05-04 22:24:39 +1000

    add genre card

--- reco-frontend/src/components/genre/AllGenres.js
+++ reco-frontend/src/components/genre/AllGenres.js
@@ -1,10 +1,8 @@
+import { GenreCard } from "./GenreCard";
 import { useEffect, useState } from "react";
-import { useHistory } from "react-router";
 
 export const AllGenres = () => {
   const [genres, setGenres] = useState([]);
-
-  const history = useHistory();
 
   useEffect(() => {
     fetch("/api/genres", {
@@ -26,23 +24,7 @@ export const AllGenres = () => {
       <div className="header-text" id="header-categories">
         <h1>Genres</h1>
       </div>
-      <ul>
-        {genres.map((el) => (
-          <div className="user-recos-list" id="genre-list-hoverable" >
-            <div id="genre-list" className={`${el.name}-category`} onClick={() => history.push(`/${el.name}-genre`)}>
-              <li key={el._id}>
-                <h5 className="reco-name">{el.name}</h5>
-                <img className="reco-img" src={el.img} alt=""></img>
-                {/* <br />
-                Category: {el.category}
-                <br />
-                Source/Author: {el.source}
-                <br /> */}
-              </li>
-            </div>
-          </div>
-        ))}
-      </ul>
+      <GenreCard listToMap={genres}/>
     </div>
   );
 };

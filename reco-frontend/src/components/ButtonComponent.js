f226062358cc9be7e143b70c926e79d193991db9
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-05-06 13:18:33 +1000

    fix issues to remove warnings

--- reco-frontend/src/components/ButtonComponent.js
+++ reco-frontend/src/components/ButtonComponent.js
@@ -1,5 +1,5 @@
 import Button from "@material-ui/core/Button";
-import { withStyles } from "@material-ui/core/styles";
+// import { withStyles } from "@material-ui/core/styles";
 
 export const ButtonComponent = (props) => {
   return (
--- reco-frontend/src/components/Navbar.js
+++ reco-frontend/src/components/Navbar.js
@@ -3,109 +3,76 @@ import {
   AppBar,
   Toolbar,
   IconButton,
-  // List,
-  // ListItem,
-  // ListItemText,
   Container,
   Button,
 } from "@material-ui/core";
 import { AccountCircle, Home, Search } from "@material-ui/icons";
 import { useHistory } from "react-router";
 
-// const navLinks = [
-//   { name: "Search", path: "/search" },
-//   { name: "Clubs", path: "/clubs" },
-//   { name: "Categories", path: "/categories" },
-//   { name: "About", path: "/about" },
-// ];
-
-// const navLinksLoggedIn = [
-//   { name: "Search", path: "/search" },
-//   { name: "Clubs", path: "/clubs" },
-//   { name: "Categories", path: "/categories" },
-//   { name: "My Reco's", path: "/my-recos" },
-//   { name: "My List", path: "/my-list" },
-//   { name: "Sign Out", path: "/" },
-// ]
-
 export const Navbar = (props) => {
-
   const history = useHistory();
 
   const currentUserToken = window.localStorage.getItem("token");
-
-
 
   const homeClickHandler = () => {
     history.replace("/");
   };
 
   const searchClickHandler = () => {
-  history.replace("/search");
-};
+    history.replace("/search");
+  };
 
-const clubsClickHandler = () => {
-history.replace("/all-clubs");
-};
+  const clubsClickHandler = () => {
+    history.replace("/all-clubs");
+  };
 
-const categoriesClickHandler = () => {
-history.replace("/categories");
-};
+  const categoriesClickHandler = () => {
+    history.replace("/categories");
+  };
 
-const myRecosClickHandler = () => {
-history.replace("/my-recos");
-};
+  const myRecosClickHandler = () => {
+    history.replace("/my-recos");
+  };
 
-const myListClickHandler = () => {
-history.replace("/my-list");
-};
+  const myListClickHandler = () => {
+    history.replace("/my-list");
+  };
 
-const myClubsClickHandler = () => {
-  history.replace("/my-club");
+  const myClubsClickHandler = () => {
+    history.replace("/my-club");
   };
 
   const genresClickHandler = () => {
     history.replace("/genres");
-    };
+  };
 
-const aboutClickHandler = () => {
-history.replace("/about");
-};
+  const aboutClickHandler = () => {
+    history.replace("/about");
+  };
 
   const accountClickHandler = () => {
-  history.replace("/dashboard");
-};
+    history.replace("/dashboard");
+  };
 
-const signOutClickHandler = () => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("id");
-  localStorage.removeItem("username");
+  const signOutClickHandler = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+    localStorage.removeItem("username");
 
-  props.setLoginStatus(false)
-  history.replace("/");
-};
+    props.setLoginStatus(false);
+    history.replace("/");
+  };
 
-const signInClickHandler = () => {
-  history.replace("/login");
-};
+  const signInClickHandler = () => {
+    history.replace("/login");
+  };
 
-const registerClickHandler = () => {
-  history.replace("/register");
-};
+  const registerClickHandler = () => {
+    history.replace("/register");
+  };
 
-// React.useEffect(() => {
-//   const isToken = localStorage.getItem('token');
-//   console.log(isToken)
-  
-
-//   setIsAuthenticated(isToken);
-//   console.log(isAuthenticated)
-
-// }, []);
-
-return (
-  
-<AppBar position="static">
+  return (
+    <AppBar position="static">
       <Toolbar className="navbar">
         <Container className="navbar-display-flex">
           <IconButton
@@ -114,57 +81,65 @@ return (
             className="home"
             onClick={homeClickHandler}
           >
-            <Home fontSize="large"/>
+            <Home fontSize="large" />
           </IconButton>
-
           <IconButton
             edge="start"
             color="inherit"
             className="home"
             onClick={searchClickHandler}
-
           >
-            <Search fontSize="large"/>
+            <Search fontSize="large" />
           </IconButton>
-
-          <Button color="inherit" onClick={clubsClickHandler}>Clubs</Button>
-          <Button color="inherit" onClick={genresClickHandler}>Genres</Button>
-          <Button color="inherit" onClick={categoriesClickHandler}>Categories</Button>
-
-
-          {(currentUserToken === "undefined") || (currentUserToken === null) && (<span>
-            <Button color="inherit" onClick={aboutClickHandler}>About</Button>
-          <Button color="inherit" onClick={registerClickHandler}>Register</Button>
-          <Button color="inherit" onClick={signInClickHandler}>Sign In</Button></span>)}
-
-
-          {props.loginStatus && (<span>
-            <Button color="inherit" onClick={myClubsClickHandler}>My Clubs</Button>
-            <Button color="inherit" onClick={myRecosClickHandler}>My Reco's</Button>
-          <Button color="inherit" onClick={myListClickHandler}>My List</Button>
-          <Button color="inherit" onClick={signOutClickHandler}>Sign Out</Button></span>)}
-
-
-          
-
+          <Button color="inherit" onClick={clubsClickHandler}>
+            Clubs
+          </Button>
+          <Button color="inherit" onClick={genresClickHandler}>
+            Genres
+          </Button>
+          <Button color="inherit" onClick={categoriesClickHandler}>
+            Categories
+          </Button>
+          {(currentUserToken === "undefined" ||
+            currentUserToken === null) && (
+              <span>
+                <Button color="inherit" onClick={aboutClickHandler}>
+                  About
+                </Button>
+                <Button color="inherit" onClick={registerClickHandler}>
+                  Register
+                </Button>
+                <Button color="inherit" onClick={signInClickHandler}>
+                  Sign In
+                </Button>
+              </span>
+            )}
+          {props.loginStatus && (
+            <span>
+              <Button color="inherit" onClick={myClubsClickHandler}>
+                My Clubs
+              </Button>
+              <Button color="inherit" onClick={myRecosClickHandler}>
+                My Reco's
+              </Button>
+              <Button color="inherit" onClick={myListClickHandler}>
+                My List
+              </Button>
+              <Button color="inherit" onClick={signOutClickHandler}>
+                Sign Out
+              </Button>
+            </span>
+          )}
           <IconButton
             edge="end"
             color="inherit"
             className="account"
             onClick={accountClickHandler}
           >
-            <AccountCircle fontSize="large"/>
+            <AccountCircle fontSize="large" />
           </IconButton>
         </Container>
       </Toolbar>
     </AppBar>
-
-
-)
-
-
-    
-
-    
-
-  }
+  );
+};

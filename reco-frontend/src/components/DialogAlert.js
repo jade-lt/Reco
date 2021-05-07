7265e84a965efd6447163ad7484c952ecb3795d5
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-05-07 12:46:16 +1000

    create dialog component

---
+++ reco-frontend/src/components/DialogAlert.js
@@ -0,0 +1,21 @@
+import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@material-ui/core";
+
+
+export const DialogAlert = (props) => {
+
+
+return (
+
+    <Dialog open={props.open}>
+        <DialogTitle>{props.title}</DialogTitle>
+        <DialogContent>
+        <DialogContentText>{props.description}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+        <Button onClick={props.onCancelClick}>Cancel</Button>
+            <Button onConfirmClick={props.onConfirmClick}>{props.confirmButtonLabel}</Button>
+        </DialogActions>
+    </Dialog>
+)
+
+}

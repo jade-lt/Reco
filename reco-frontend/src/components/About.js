064299b36b22d0e0bc72103e76b26bb46eaad0d9
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-05-08 10:36:14 +1000

    add about info

--- reco-frontend/src/components/About.js
+++ reco-frontend/src/components/About.js
@@ -1,7 +1,30 @@
+import { ButtonComponent } from "./ButtonComponent";
+import { useHistory } from "react-router";
+
+
 export const About = () => {
+
+  const history = useHistory();
+
     return (
-        <div>
-            <h1>This is the about page</h1>
+        <div className="main">
+            <div className="about-info">
+            <h1>About Reco</h1>
+            <h5>Reco is a place to find and share<br/> recommendations of Movies, TV Shows, Books & Games.
+                <br />
+            </h5>
+            <h3>How it Works</h3>
+            <h5>If you just want to browse, you don't<br/> need to sign up, you can wander through categories<br/> and genres to find recommendations <br/>of things you're interested in!
+                <br />
+            </h5>
+            <h3>Join Reco!</h3>
+            <h5>If you'd like to make recommendations <br/>of all of your favourite things and share them with others,<br/> sign up to Reco for free!
+            <br/>You'll also be able to save other user's recommendations<br/> to a list to find later when you're looking <br/>for ideas for something to read, watch or play.
+            </h5>
+            </div>
+            <div>
+                <ButtonComponent buttonLabel="Join Reco Now" onClick={() => history.push("./register")}/>
+            </div>
         </div>
     )
 }
